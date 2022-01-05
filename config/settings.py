459367a5d3663e8fa2b60e37693d324d0f412940
Andres Rivero <andres19rivero@gmail.com>
2022-01-05 00:23:02 -0400

    New updates for Heoku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vedf69gxosgg^orlv@fizlnhwtqfm^j($3g$%52nn^5)3=ricz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
